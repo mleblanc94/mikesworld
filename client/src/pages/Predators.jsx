61b819c201bd840af2deee3510ba3e9f473312a6
unknown <leblancmichael94@gmail.com>
2026-01-31 23:39:45 -0500

    made more changes to the category pages

--- client/src/pages/Predators.jsx
+++ client/src/pages/Predators.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Predators = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('api/posts');
+                if (!response.ok) {
+                    throw new Error("Error getting the posts from the API!");
+                }
+                const result = response.json();
+                setPosts(result)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    })
+
+    const predatorPosts = posts.filter((post) => post.category === "predators");
     return(
         <div>
             <h1>Predators placeholder</h1>
+            {pedatorsPost.length > 0 ? () : ()}
         </div>
     )
 }
--- client/src/pages/PublicFreakouts.jsx
+++ client/src/pages/PublicFreakouts.jsx
@@ -4,10 +4,41 @@ const PublicFreakouts = () => {
 
     const [posts, setPosts] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+            const response = await fetch('/api/posts');
+            if (!response.ok) {
+                throw new Error('Error pulling from the API');
+            }
+            const result = response.json();
+            setPosts(result);
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchData();
+    }, [])
+
+
+    const publicFreakoutCategory = posts.filter((post) => post.category === "public-freakouts")
 
     return(
         <div>
             <h1>Public Freakouts placeholder</h1>
+            {publicFreakoutCategory.length > 0 ? (
+                publicFreakoutCategory.map((post) => {
+                    return (
+                        <div className='post-cards' key={post._id}>
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                            <h6>{post.category}</h6>
+                        </div>
+                    )
+                })
+            ) : (
+                <p>No posts for this category!</p>
+            )}
         </div>
     )
 }
