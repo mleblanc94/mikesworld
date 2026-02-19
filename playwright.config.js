3dc9d90e7dabf6a2f0f29ace2935163b2514ab82
unknown <leblancmichael94@gmail.com>
2026-02-18 21:47:08 -0500

    made minor changes to page files and added testing

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+// playwright.config.js
+const { defineConfig } = require("@playwright/test");
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: true,
+    timeout: 120 * 1000,
+  },
+});
